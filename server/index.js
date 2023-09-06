84c3473b0668e6780b70cd833cf4d63b2d22a002
Tomer <tomerrim@gmail.com>
2023-09-06 13:45:04 +0300

    changed server.js to index.js and moved it to the root folder of server

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ import dotenv from "dotenv"
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import userRouter from "../src/routes/userRoute.js";
-import seedRouter from "../src/routes/seedRoute.js";
-import orderRouter from "../src/routes/orderRoute.js";
-import productRouter from "./routes/productRoute.js";
+import userRouter from "./src/routes/userRoute.js";
+import seedRouter from "./src/routes/seedRoute.js";
+import orderRouter from "./src/routes/orderRoute.js";
+import productRouter from "./src/routes/productRoute.js";
 
 dotenv.config();
 
